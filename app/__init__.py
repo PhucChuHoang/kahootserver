926454639f1aecf0d6160552896c4e0f8604a13c
tcm03 <21125019@student.hcmus.edu.vn>
2024-05-18 13:33:46 +0700

    Add flask cors

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +12,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
 socketio = SocketIO(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 blacklist = set()
 
